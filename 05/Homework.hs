fa19a70fc1c22886b1842720689396c83db9f948
landcelita <nyoo.www@gmail.com>
2024-05-20 03:23:13 +0900

    5.3

--- 05/Homework.hs
+++ 05/Homework.hs
@@ -11,3 +11,16 @@ evalStr x =
     case parseExp Lit Add Mul x of
       Just expr -> Just (eval expr)
       Nothing -> Nothing
+
+class Expr a where
+  mul :: a -> a -> a
+  add :: a -> a -> a
+  lit :: Integer -> a
+
+instance Expr ExprT where
+  mul x y = Mul x y
+  add x y = Add x y
+  lit x = Lit x
+
+reify :: ExprT -> ExprT
+reify = id
