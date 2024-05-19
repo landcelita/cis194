4b65cc3abe22c33bcc7d9afb55c88111e4f64de1
landcelita <nyoo.www@gmail.com>
2024-05-20 03:53:55 +0900

    5.4

--- 05/Homework.hs
+++ 05/Homework.hs
@@ -22,5 +22,32 @@ instance Expr ExprT where
   add x y = Add x y
   lit x = Lit x
 
-reify :: ExprT -> ExprT
-reify = id
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr Integer where
+  mul x y = x * y
+  add x y = x + y
+  lit x = x
+
+instance Expr Bool where
+  mul x y = x && y
+  add x y = x || y
+  lit x = x > 0
+
+instance Expr MinMax where
+  mul (MinMax x) (MinMax y) = MinMax(min x y)
+  add (MinMax x) (MinMax y) = MinMax(max x y)
+  lit x = MinMax(x)
+
+instance Expr Mod7 where
+  mul (Mod7 x) (Mod7 y) = Mod7 ((x * y) `mod` 7)
+  add (Mod7 x) (Mod7 y) = Mod7 ((x + y) `mod` 7)
+  lit x = Mod7(x)
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+testInteger = testExp :: Maybe Integer
+testBool = testExp :: Maybe Bool
+testMM = testExp :: Maybe MinMax
+testSat = testExp :: Maybe Mod7
